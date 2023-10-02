772c44dc8530214312b59ba63734fbb73e844d3d
MatveevIl <ilmatveev05@gmail.com>
2023-10-02 23:46:37 +0300

    dfhaeihgis

---
+++ My project (2)/Assets/Myscript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Myscript : MonoBehaviour
+{
+    public GameObject sphere;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            Debug.Log("Object is activated");
+            sphere.SetActive(true);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            Debug.Log("Object is deactivated");
+            sphere.SetActive(false);
+        }
+
+    }
+}
